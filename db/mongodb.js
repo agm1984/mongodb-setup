ab8f5698232d2b59b607bd24fb2b610128d919c2
Adam Mackintosh <agm1984@gmail.com>
2017-08-25 22:12:03 -0700

    fixed garbo entrails

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,16 +1,14 @@
 const MongoClient = require('mongodb').MongoClient
-const uri = 'mongodb://mongodb:E1atHome@localhost:27017/PAC'
+const uri = 'mongodb://user:password@localhost:27017/PAC'
 let _db
 
 const connectDB = async (callback) => {
     try {
-        console.log('here')
         MongoClient.connect(uri, (err, db) => {
             _db = db
             return callback(err)
         })
     } catch (e) {
-        console.log('rere')
         throw e
     }
 }
